ce3b357804fcb98cb3cb2b9d0a748cc9975ff49d
snufkin12 <strzalkafilip@gmail.com>
2020-12-15 16:14:36 +0100

    added PyPI configuration and new trained model

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sentimentpl",
+    version="0.0.1",
+    author="Filip Strzałka",
+    author_email="strzalkafilip@gmail.com",
+    description="PyTorch models for polish language sentiment regression based on allegro/herbert and CLARIN-PL dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/philvec/sentimentPL",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['torch', 'transformers', 'tqdm', 'matplotlib']
+)
