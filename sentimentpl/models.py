0f870dcc4ab887038071129581a9676e85f6a5e4
snufkin12 <strzalkafilip@gmail.com>
2021-01-19 17:20:31 +0100

    model better trained to MSE ~0.307, added HerBERT finetuning option, save path bug fixes

--- sentimentpl/models.py
+++ sentimentpl/models.py
@@ -14,11 +14,14 @@ except ImportError:
 from sentimentpl import trained_models
 
 class SentimentPLModel(nn.Module):
-    def __init__(self, from_pretrained=None):
+    def __init__(self,
+                 from_pretrained=None,
+                 tokenizer="allegro/herbert-klej-cased-tokenizer-v1",
+                 embed_model="allegro/herbert-klej-cased-v1"):
         super().__init__()
 
-        self.tokenizer = XLMTokenizer.from_pretrained("allegro/herbert-klej-cased-tokenizer-v1")
-        self.embed_model = RobertaModel.from_pretrained("allegro/herbert-klej-cased-v1", return_dict=True)
+        self.tokenizer = XLMTokenizer.from_pretrained(tokenizer)
+        self.embed_model = RobertaModel.from_pretrained(embed_model, return_dict=True)
 
         self.fc = nn.Sequential(nn.Dropout(0.5),
                                 nn.Linear(768, 256), nn.ReLU(),
@@ -30,17 +33,18 @@ class SentimentPLModel(nn.Module):
             self.fc.load_state_dict(torch.load(f))
             self.eval()
 
-    def save(self, name='latest.pth'):
+    def save(self, name='latest'):
         self.fc.to('cpu')
         torch.save(self.fc.state_dict(), f'./trained_models/{name}.pth')
         self.fc.to(next(self.embed_model.parameters()).device)
 
     def forward(self, x, tune_embedding=False):
+        encoded = self.tokenizer(x, return_tensors='pt', padding=True)
+        encoded = {k: v.to(next(self.parameters()).device) for k, v in encoded.items()}
+
         with ExitStack() as stack:
             if not tune_embedding:
                 stack.enter_context(torch.no_grad())
-            encoded = self.tokenizer(x, return_tensors='pt', padding=True)
-            encoded = {k: v.to(next(self.parameters()).device) for k, v in encoded.items()}
             embeddings = self.embed_model(**encoded)['pooler_output'].float()
 
         return self.fc(embeddings)
